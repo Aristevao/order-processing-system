9c71874299fb84b7bc2fbd697c2ce0282587248d
vntarto <aristevao.santos@venturus.org.br>
2025-03-14 16:15:00 -0300

    Create endpoint to test order

--- order-service/src/main/java/com/ops/orderservice/OrderController.java
+++ order-service/src/main/java/com/ops/orderservice/OrderController.java
@@ -4,13 +4,16 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RestController
 @RequestMapping("/orders")
 @RequiredArgsConstructor
@@ -28,5 +31,11 @@ public class OrderController {
         } catch (JsonProcessingException e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro ao processar pedido.");
         }
+    }
+
+    @GetMapping
+    public ResponseEntity<String> greeting() {
+        log.info("Received request: GET /orders");
+        return ResponseEntity.ok("Hello World");
     }
 }
